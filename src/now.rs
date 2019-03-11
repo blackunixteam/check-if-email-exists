db011620441e9b2746d1b3f5643d4f7f97b0d0f7
Amaury Martiny <amaury.martiny@protonmail.com>
2019-03-11 22:35:03 +0100

    Move to now/index.ts

--- src/now.rs
+++ src/now.rs
@@ -1,42 +1,11 @@
-// Try first with simple example
+use http::{header, Request, Response, StatusCode};
 
-use http::{Request, Response, StatusCode, header};
-
- fn handler(request: Request<()>) -> http::Result<Response<String>> {
-    let response = Response::builder()
+fn handler(request: Request<()>) -> http::Result<Response<String>> {
+	let response = Response::builder()
         .status(StatusCode::OK)
         .header(header::CONTENT_TYPE, "text/html")
         .body("<!doctype html><html><head><title>A simple deployment with Now!</title></head><body><h1>Welcome to Rust on Now</h1></body></html>".to_string())
         .expect("failed to render response");
 
-     Ok(response)
+	Ok(response)
 }
-
-// extern crate check_if_email_exists;
-
-// use check_if_email_exists::email_exists;
-// use http::{header, Request, Response, StatusCode, Uri};
-// use std::collections::HashMap;
-// use url::Url;
-
-// fn handler(request: Request<()>) -> http::Result<Response<String>> {
-// 	let uri_str = request.uri().to_string();
-// 	let url = Url::parse(&uri_str).unwrap();
-
-// 	// Create a hash map of query parameters
-// 	let hash_query: HashMap<_, _> = url.query_pairs().to_owned().collect();
-
-// 	if let hash_query.get("to_email") = Some(ref to_email) {
-// 		let from_email = hash_query.get("from_email").or_else("user@example.org");
-
-// 		let response = Response::builder()
-// 			.status(StatusCode::OK)
-// 			.header(header::CONTENT_TYPE, "application/json")
-// 			.body(email_exists(from_email, to_email).to_string())
-// 			.expect("Failed to render response");
-// 	} else {
-// 		Response::builder()
-// 			.status(StatusCode::BAD_REQUEST)
-// 			.body("`to_email` is a required query param".to_string())
-// 	}
-// }
---
+++ src/now/index.rs
@@ -0,0 +1,32 @@
+extern crate check_if_email_exists;
+
+use check_if_email_exists::email_exists;
+use http::{header, Request, Response, StatusCode};
+use std::borrow::Cow;
+use std::collections::HashMap;
+use url::Url;
+
+fn handler(request: Request<()>) -> http::Result<Response<String>> {
+	let uri_str = request.uri().to_string();
+	let url = Url::parse(&uri_str).unwrap();
+
+	// Create a hash map of query parameters
+	let hash_query: HashMap<_, _> = url.query_pairs().to_owned().collect();
+
+	if let Some(ref to_email) = hash_query.get("to_email") {
+		let from_email = hash_query
+			.get("from_email")
+			.unwrap_or(&Cow::Borrowed("user@example.org"));
+		let response = Response::builder()
+			.status(StatusCode::OK)
+			.header(header::CONTENT_TYPE, "application/json")
+			.body(email_exists(from_email, to_email).to_string())
+			.expect("Failed to render response");
+
+		Ok(response)
+	} else {
+		Response::builder()
+			.status(StatusCode::BAD_REQUEST)
+			.body("`to_email` is a required query param".to_string())
+	}
+}
